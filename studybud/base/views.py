bd11adfb9dacbeae8b56bddf814fc6ada799b3eb
Jhony <scostajhony@gmail.com>
2024-08-19 16:33:39 -0300

    Finalizando o CRUD, adicionado botao de deletar

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -22,7 +22,7 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
-    
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -32,4 +32,24 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance = room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
 
