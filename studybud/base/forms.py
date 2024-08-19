bdeb184029696f0387a4da63853e553e589931c4
Jhony <scostajhony@gmail.com>
2024-08-19 16:02:13 -0300

    Iniciando CRUD para criação das salas e colocando ordenação nas salas com updated e created

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from .models import Room
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        field = '__all__'
+        fields = '__all__'
